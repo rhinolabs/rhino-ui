8760d6035df1b5d8750e12390034a3343a5decab
Christian Llontop <chrisllontop@icloud.com>
2024-12-04 21:41:04 -0500

    Add hooks and react dependencies

---
+++ packages/hooks/src/hooks/useGeolocation.ts
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+interface Payload {
+	lat: number;
+	lng: number;
+}
+
+export function useGeolocation(defaultPosition: Payload | null = null) {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [position, setPosition] = useState<Payload | null>(defaultPosition);
+	const [error, setError] = useState<string | null>(null);
+
+	function getPosition(): void {
+		if (!navigator.geolocation) {
+			setError("Your browser does not support geolocation");
+			return;
+		}
+
+		setIsLoading(true);
+		navigator.geolocation.getCurrentPosition(
+			(pos) => {
+				setPosition({
+					lat: pos.coords.latitude,
+					lng: pos.coords.longitude,
+				});
+				setIsLoading(false);
+			},
+			(error) => {
+				setError(error.message);
+				setIsLoading(false);
+			},
+		);
+	}
+
+	return { isLoading, position, error, getPosition };
+}
